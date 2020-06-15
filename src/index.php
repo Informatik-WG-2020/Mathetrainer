1c56272f1bbd3f2d1fe3b34f13b18bfb7ef5180a
Informatik-WG-2020 <super.krasser.webshop@gmail.com>
2020-06-15 09:15:36 +0200

    Add new index and impressum files

--- src/index.php
+++ src/index.php
@@ -1,7 +1,17 @@
-<?php
-
-
-echo("Hello world!");
-
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Mathetrainer: Startseite</title>
+	</head>
+	<body>
+		<h1>Mathetrainer</h1>
+		<p>Herzlich Willkommen zum Mathetrainer des Informatik-Vierstündig-Kurses 2020 am WG.<br />
+		Teste unsere verschiedenen Aufgabentypen und übe deine Mathe-Grundkenntnisse! Viel Spaß und viel Erfolg!<br />
+		Dieses Projekt wurde im Informatik-Kurs entwickelt vom 15.06.2020 bis XX.07.2020.</p>
+		
+		<div name="links">
+			<a href="impressum.html"><button>Impressum!</button></a>
+		</div>
+	</body>
+</html>
